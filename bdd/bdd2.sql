c543c6258948ec78487d3ba9f4ad53cf35350159
he-lau <he.laurent.1130@gmail.com>
2023-11-13 00:02:14 +0100

    database model v2

---
+++ bdd/bdd2.sql
@@ -0,0 +1,35 @@
+CREATE TABLE `user` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+);
+
+CREATE TABLE `user_movie_relation` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `movie_id` int NOT NULL,
+  `movie_is_from_API` tinyint(1) NOT NULL DEFAULT "0",
+  `rating` int,
+  `favourite` tinyint(1) NOT NULL DEFAULT "0",
+  `user_humor` enum,
+  `user_location` varchar(255),
+  `user_location_weather` varchar(255),
+  `form_submission_date` datetime NOT NULL,
+  `rating_timestamp` datetime NOT NULL
+);
+
+CREATE TABLE `movie` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255)
+);
+
+CREATE TABLE `API_ref` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `url` varchar(255),
+  `label` varchar(50)
+);
+
+ALTER TABLE `user_movie_relation` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `user_movie_relation` ADD FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`);
