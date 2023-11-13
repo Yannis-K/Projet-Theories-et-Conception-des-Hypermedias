6660f75aed13e0068a435f5a01899473597eb33c
he-lau <he.laurent.1130@gmail.com>
2023-11-13 09:25:21 +0100

    rename files names

--- bdd/bdd.sql
+++ bdd/bdd.sql
@@ -1,28 +1,46 @@
 CREATE TABLE `user` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `genre` tinyint NOT NULL,
   `password` varchar(255) NOT NULL
 );
 
 CREATE TABLE `user_movie_relation` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int NOT NULL,
-  `movie_api_id` int NOT NULL,
-  `rating` int,
-  `favourite` tinyint NOT NULL DEFAULT "0"
+  `movie_id` int NOT NULL,
+  `movie_is_from_API` tinyint(1) NOT NULL DEFAULT "0",
+  `rating` tinyint,
+  `favourite` tinyint(1) NOT NULL DEFAULT "0",
+  `rating_timestamp` datetime
 );
 
-CREATE TABLE `user_form_logs` (
-  `log_id` int PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `movie` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255)
+);
+
+CREATE TABLE `API_ref` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `url` varchar(255),
+  `label` varchar(50)
+);
+
+CREATE TABLE `user_recommendation_form_logs` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int NOT NULL,
-  `submission_date` datetime NOT NULL,
-  `humor` enum,
-  `location` varchar(255),
-  `weather` varchar(255)
+  `user_humor` enum,
+  `user_location_lat` DECIMAL(10, 8),
+  `user_location_long` DECIMAL(11, 8),
+  `user_location_weather_label` enum,
+  `user_location_weather_temperature` int,
+  `form_submission_date` datetime
 );
 
 ALTER TABLE `user_movie_relation` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
-ALTER TABLE `user_form_logs` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `user_movie_relation` ADD FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`);
+
+ALTER TABLE `user_movie_relation` ADD FOREIGN KEY (`movie_id`) REFERENCES `API_ref` (`id`);
+
+ALTER TABLE `user_recommendation_form_logs` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
