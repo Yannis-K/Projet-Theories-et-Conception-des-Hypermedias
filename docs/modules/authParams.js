db0aa4fe3dc1f67ebbfb4099799f46a98963c303
he-lau <he.laurent.1130@gmail.com>
2023-11-20 09:34:00 +0100

    Test omeka login

---
+++ docs/modules/authParams.js
@@ -0,0 +1,8 @@
+export const pa = {
+    'navbar':d3.select('#navbarMain'),
+    mail: 'he.laurent.1130@gmail.com',
+    apiOmk: 'https://site.test/omeka-s/api',
+    ident: 'adt0tSjXN0GG5l9uzaYnAePHAylVWgK4',
+    key:  'Xj1pEb9lQsP3ayL1KfnTW8zhvCSofdPl'
+    //keyGitHub
+}
