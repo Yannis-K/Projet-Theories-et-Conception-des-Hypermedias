14e8948fe05f9617d7bbf8fcd0e1986108156f58
Laurent HE <65168751+he-lau@users.noreply.github.com>
2023-11-27 09:33:21 +0100

    Add files via upload

--- bdd/bdd.sql
+++ bdd/bdd.sql
@@ -8,7 +8,6 @@ CREATE TABLE `user_movie_relation` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `movie_id` int NOT NULL,
-  `movie_is_from_API` tinyint(1) NOT NULL DEFAULT "0",
   `rating` tinyint,
   `favourite` tinyint(1) NOT NULL DEFAULT "0",
   `rating_timestamp` datetime
@@ -17,7 +16,8 @@ CREATE TABLE `user_movie_relation` (
 CREATE TABLE `movie` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
-  `description` varchar(255)
+  `description` varchar(255),
+  `movie_is_from_API` tinyint(1) NOT NULL DEFAULT "0"
 );
 
 CREATE TABLE `API_ref` (
@@ -37,10 +37,18 @@ CREATE TABLE `user_recommendation_form_logs` (
   `form_submission_date` datetime
 );
 
+CREATE TABLE `user_recommendation_history` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `form_log_id` int NOT NULL,
+  `movie_id` int NOT NULL
+);
+
 ALTER TABLE `user_movie_relation` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 ALTER TABLE `user_movie_relation` ADD FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`);
 
-ALTER TABLE `user_movie_relation` ADD FOREIGN KEY (`movie_id`) REFERENCES `API_ref` (`id`);
-
 ALTER TABLE `user_recommendation_form_logs` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `user_recommendation_history` ADD FOREIGN KEY (`form_log_id`) REFERENCES `user_recommendation_form_logs` (`id`);
+
+ALTER TABLE `user_recommendation_history` ADD FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`);
